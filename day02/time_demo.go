c9558c6d9abbb4ee85f79cad2c0cb1527f59e39c
yousj <yousj@yczcjk.com>
2021-10-13 09:42:43 +0800

    feat: 日期格式化

--- day02/time_demo.go
+++ day02/time_demo.go
@@ -13,6 +13,13 @@ func main() {
 
 	fmt.Println(now.Second())
 
+	// 日期格式化 [2006-01-02 15:04:05.000] (格式有点奇葩, 必须为go语言诞生时间)
+	fmt.Println(now.Format("2006-01-02 15:04:05.000"))
+	fmt.Println(now.Format("2006/01/02 15:04:05.000"))
+	fmt.Println(now.Format("2006-01-02"))
+	fmt.Println(now.Format("2006/01/02"))
+
+	// 定时任务
 	ticker := time.Tick(time.Second)
 
 	for t := range ticker {
