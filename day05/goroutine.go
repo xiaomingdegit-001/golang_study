3ee551baf733fee92369ce656513999004c1bc59
小明 <youshaojunde@163.com>
2021-10-17 18:58:24 +0800

    feat: goroutine

--- day05/goroutine.go
+++ day05/goroutine.go
@@ -3,6 +3,29 @@ package main
 /**
  * 轻量级线程
  */
-func main() {
 
+import (
+	"fmt"
+	"sync"
+)
+
+// 计数器, 类似Java的CountDownLatch
+var wg = sync.WaitGroup{}
+
+func test() {
+	// 计数器减1
+	defer wg.Done()
+	fmt.Println("hello goroutine.")
+}
+
+func main() {
+	var num = 10
+	wg.Add(num)
+	for i := 0; i < num; i++ {
+		// 使用go关键字, 启动goroutine, 执行test()函数
+		go test()
+	}
+	fmt.Println("hello main.")
+	// 阻塞等待结束
+	wg.Wait()
 }
