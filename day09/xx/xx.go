502f410d7c5d6cab028b3492271844152e9fbaff
小明 <youshaojunde@163.com>
2021-10-17 21:33:47 +0800

    feat: 单元测试

---
+++ day09/xx/xx.go
@@ -0,0 +1,7 @@
+package xx
+
+import "fmt"
+
+func Xxx() {
+	fmt.Println("hello test.")
+}
---
+++ day09/xx/xx_unit_test.go
@@ -0,0 +1,15 @@
+package xx
+
+import (
+	"testing"
+)
+
+/**
+ * 单元测试, 文件以_test.go 结尾
+ *     1. 单元测试, 函数名为TestXxx
+ * 	   2. 基准测试
+ * 	   3. 示例函数
+ */
+func TestXxx(t *testing.T) {
+	Xxx()
+}
