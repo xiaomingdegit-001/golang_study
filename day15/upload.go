9723bee0e106213458dabe26e65d24e7f4d8b8c7
小明 <youshaojunde@163.com>
2021-10-23 21:25:45 +0800

    feat: gin文件上传

---
+++ day15/upload.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/**
+ * gin 文件上传
+ */
+
+func ok(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"msg":  "ok",
+	})
+}
+
+func fail500(context *gin.Context, err error) {
+	context.JSON(http.StatusInternalServerError, gin.H{
+		"code": http.StatusInternalServerError,
+		"msg":  err,
+	})
+}
+
+func uploadHandler(context *gin.Context) {
+	file, err := context.FormFile("file")
+	if err != nil {
+		fail500(context, err)
+		return
+	}
+	err = context.SaveUploadedFile(file,
+		fmt.Sprintf("./day15/static/images/%s", file.Filename))
+	if err != nil {
+		fail500(context, err)
+		return
+	}
+	ok(context)
+}
+
+func main() {
+	engine := gin.Default()
+	engine.LoadHTMLGlob("./day15/template/**")
+	engine.GET("/upload", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "upload.html", nil)
+	})
+	engine.POST("/upload", uploadHandler)
+	engine.Run()
+}
